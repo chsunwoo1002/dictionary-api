4f01c839a77cdaa2d8165521640de8981431b2c1
chsunwoo1002 <chsunwoo1002@gmail.com>
2022-02-15 20:59:24 -0800

    add app valid HTTP request testcases

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,6 +1,7 @@
 import pytest
 from app import app
 from modules.utils import *
+import json
 
 @pytest.fixture(scope='module')
 def client():
@@ -16,3 +17,17 @@ def testAppUsage(client):
   response = client.get('/usage')
   assert response.status_code == 200
   assert response.data.decode("utf-8") == usageMessage
+
+def testValidRequest(client):
+  response = client.get('/dictionary-api/v1/?word=apple&language=en-US')
+  assert response.status_code == 200
+  data = json.loads(response.data.decode('utf-8'))
+  assert "meaning" in data
+  assert "word" in data
+  assert "type" in data
+  assert "phonetics" in data
+  assert "definitions" in data["meaning"][0]
+  assert "partOfSpeech" in data["meaning"][0]
+  assert "definition" in data["meaning"][0]["definitions"][0]
+  assert "examples" in data["meaning"][0]["definitions"][0]
+  assert "text" in data["phonetics"][0]
