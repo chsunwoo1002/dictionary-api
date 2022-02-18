2eb53f4221a2dd433900cf53c10c2df78dd3fc8d
chsunwoo1002 <chsunwoo1002@gmail.com>
2022-02-17 21:22:12 -0800

    add codes to catch HTTP errors

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
+from curses.ascii import HT
 from distutils.debug import DEBUG
 from flask import request, jsonify, Flask
 from modules.googleDictionary import queryWord
 from modules.utils import *
-from werkzeug.exceptions import HTTPException
+from werkzeug.exceptions import HTTPException, UnprocessableEntity, BadRequest
 
 app = Flask(__name__)
 app.config.from_mapping(
@@ -17,11 +18,10 @@ def home():
 @app.route('/usage', methods=['GET'])
 def usage():
   return usageMessage
-  
+
+# Return JSON instead of HTML for HTTP errors.  
 @app.errorhandler(HTTPException)
 def handle_exception(e):
-    """Return JSON instead of HTML for HTTP errors."""
-    # start with the correct headers and status code from the error
     response = e.get_response()
     # replace the body with JSON
     response.data = json.dumps({
@@ -34,22 +34,26 @@ def handle_exception(e):
 
 @app.route('/dictionary-api/v1/', methods=['GET'])
 def apiSearch():
+  # check length of parameters is correct
+  if len(request.args) != 2:
+    raise BadRequest
+
   # Check if an search was provided as part of the URL.
   # If search is provided, assign it to a variable.
   # If no search is provided, display an error in the browser.
-  if 'word' in request.args:
+  if 'word' in request.args and request.args['word'] != '':
     searchWord = request.args['word']
   else:
-    raise 'raise unprocessableEntity()'
+    raise UnprocessableEntity
 
   # Check if an language was provided as part of the URL.
   # If language is provided, assign it to a variable.
   # If no language is provided, display an error in the browser.
-  if 'language' in request.args:
+  if 'language' in request.args and request.args['language'] != '':
     language = request.args['language']
   else:
-    raise 'return unprocessableEntity()'
-
+    raise UnprocessableEntity
+    
   # get JSON which is about searching word
   # If it successfully finds, it returns "data" in type key
   # If it does not successfully find, it returns "error" in type key
